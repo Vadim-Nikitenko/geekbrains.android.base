62e9968fa75eaf46eba4eb6ecb29d4d2730e56cb
Vadim-Nikitenko <nikagw24@gmail.com>
2020-07-31 09:52:02 +0300

    Lesson3

--- app/src/main/java/com/example/geekbrainsandroidweather/MainActivity.java
+++ app/src/main/java/com/example/geekbrainsandroidweather/MainActivity.java
@@ -1,14 +1,54 @@
 package com.example.geekbrainsandroidweather;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    private TextView temperature;
+    private Button increaseTemperatureBtn;
+    private final String counterDataKey = "counterDataKey";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        setOnIncreaseTemperatureBtnBehaviour();
+    }
+
+    private void init() {
+        temperature = findViewById(R.id.temperature);
+        increaseTemperatureBtn = findViewById(R.id.increaseTemperatureBtn);
+    }
+
+    private void setOnIncreaseTemperatureBtnBehaviour() {
+        increaseTemperatureBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int currentTemperature = Integer.parseInt(temperature.getText().toString()
+                        .replace("°", ""));
+                String newTemperature = ++currentTemperature + "°";
+                temperature.setText(newTemperature);
+            }
+        });
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle saveInstanceState) {
+        String text = temperature.getText().toString();
+        saveInstanceState.putString(counterDataKey, text);
+        super.onSaveInstanceState(saveInstanceState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        String text = savedInstanceState.getString(counterDataKey);
+        temperature.setText(text);
     }
 }
